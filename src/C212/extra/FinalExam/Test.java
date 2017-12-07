636d1c580260c829e8a1a41d57e100e3b9f7b670
Josh Isaacson <jsisaacs@iu.edu>
2017-12-07 18:11:50 -0500

    finished roughly 80/150 review problems so far

--- src/C212/extra/FinalExam/Test.java
+++ src/C212/extra/FinalExam/Test.java
@@ -1,30 +1,14 @@
 package C212.extra.FinalExam;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        LinkedList<String> words = new LinkedList();
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
+        Scanner in = new Scanner(System.in);
+        String word = in.next();
+        System.out.println(word);
 
-        System.out.println(words);
-
-        ListIterator<String> iter = words.listIterator();
-
-        while (iter.hasNext()) {
-            String string = iter.next();
-            if ((words.indexOf(string) % 2) == 1) {
-                System.out.println(string);
-            }
-        }
-
-
-
+        String input = in.nextLine();
+        System.out.println(input);
     }
 }
